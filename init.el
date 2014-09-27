5bf24c29f7a29cea368da035b721e84a96244b94
Tim Morgan <tim@timmorgan.org>
2014-09-27 16:31:04 -0500

    Open and navigate between buffers
    This is a huge installment in the series -- one in which I start to
    really make this mimic my existing Vim setup.
    
    (Yeah yeah, I know some of you are saying, "Why are you just making
    Emacs behave like Vim? That's missing the point." Because I know for
    my switch to Emacs to have any permanence, I need to bootstrap my brain
    into it, i.e. I need to be productive on Monday. Change is ok;
    starting over as a n00b is not. Don't worry, I'll learn more Emacs-y
    ways of doing stuff with time. Besides... the main reason I want to try
    Emacs again is because I have my own special notions of how I want my
    editor to behave and want to customize.)
    
    In this commit, I learned "just enough" about Helm and Projectile so I
    can quickly open files like I did with my beloved CtrlP vim plugin. I
    know I'm missing A LOT of what Helm can do, but I'll just have to come
    back to it later.
    
    My goal here was to get my four main buffer navigation keys working:
    
    ,r  open a recent file
    ,t  open a file in the current project
    ,y  switch to a different buffer
    ,/  switch to the last open file (similar to Vim's "alternate-file")
    
    The first three are now very close, and the last one is much
    better than I had hoped.
    
    Whereas Vim's alternate-file would only toggle between the last
    two files, iflipb (which I have mapped to <leader>/) works more like
    alt-tab on my Mac, and it rocks. So yay.
    
    You'll notice I installed evil-leader so I can create some familiar
    <leader> key bindings.
    
    There is still much more to do/learn here, but I'm happy with the
    progress and can see myself actually maybe using this editor on Monday
    when I get back to doing "real" work. Though, I need just a few more
    things... stay tuned!

--- init.el
+++ init.el
@@ -9,9 +9,14 @@
 ;; Guarantee all packages are installed on start
 (defvar packages-list
   '(evil
+    evil-leader
     color-theme
     color-theme-solarized
-    git-commit-mode)
+    git-commit-mode
+    helm
+    helm-projectile
+    grizzl
+    iflipb)
   "List of packages needs to be installed at launch")
 
 (defun has-package-not-installed ()
@@ -32,8 +37,32 @@
 ; Make yes-or-no questions answerable with 'y' or 'n'
 (fset 'yes-or-no-p 'y-or-n-p)
 
+(setq evil-leader/in-all-states 1)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
 (require 'evil)
 (evil-mode 1)
 
 (require 'color-theme-solarized)
 (color-theme-solarized-dark)
+
+(require 'helm-config)
+(helm-mode 1)
+
+(projectile-global-mode)
+(setq projectile-require-project-root nil)
+(setq projectile-completion-system 'grizzl)
+(setq projectile-enable-caching t)
+
+(require 'iflipb)
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 100)
+
+; Buffer nagiation keys
+(evil-leader/set-key
+ "r" 'projectile-recentf
+ "t" 'projectile-find-file
+ "y" 'projectile-switch-to-buffer
+ "/" 'iflipb-next-buffer)
