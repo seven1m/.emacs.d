3f879be5ef5aeb0895ac654913073e4438c8628a
Tim Morgan <tim@timmorgan.org>
2014-09-27 14:51:32 -0500

    Vim in my Emacs, this truly is Evil!
    This latest quest involved installing the Evil package so I can get some
    modal editing up in here. I hear Emacs has a package manager built-in, yay!
    
    Turns out I couldn't even figure out how to launch said package manager
    though, because for some reason on my Mac the M-x key combo was not in my
    reach. :-(
    
    It took some googling, but I figured out that my terminal was not sending
    the right signals or some such. Here's how I fixed it:
    
    http://i.imgur.com/Ocd2Nww.png
    
    Once I figured that out, I ran `list-packages` and installed Evil. And
    it was awesome! Now I can move around quickly, like in Vim, and it feels
    so much more like home!! Much happy.
    
    As I was about to commit the changes to this repo, I realized that the
    Evil packages were downloaded into the "elpa" directory, and I was about
    to commit the full source and compiled binaries of those packages, which
    seemed like a waste.
    
    Then I found this post by Chris Zheng:
    http://blog.zhengdong.me/2012/03/14/how-i-manage-emacs-packages/
    
    I deleted my "elpa" directory to get a fresh start, then I copy-pasted
    Chris' magic elisp incantation, defined my own `packages-list` variable,
    and restarted Emacs.
    
    Voilà! Now Emacs can auto-install packages. I have to say, that was so
    easy and so little code, I'm starting to get shivers.
    
    Now "elpa" is in my `.gitignore` file, and my repo is lean!

--- init.el
+++ init.el
@@ -5,5 +5,29 @@
                          ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")))
 (package-initialize)
 
+(require 'cl)
+;; Guarantee all packages are installed on start
+(defvar packages-list
+  '(evil)
+  "List of packages needs to be installed at launch")
+
+(defun has-package-not-installed ()
+  (loop for p in packages-list
+        when (not (package-installed-p p)) do (return t)
+        finally (return nil)))
+
+(when (has-package-not-installed)
+  ;; Check for new packages (package versions)
+  (message "%s" "Get latest versions of all packages...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; Install the missing packages
+  (dolist (p packages-list)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
 ; Make yes-or-no questions answerable with 'y' or 'n'
 (fset 'yes-or-no-p 'y-or-n-p)
+
+(require 'evil)
+(evil-mode 1)
