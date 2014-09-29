ef1855a2db660acb311eaa2fc50fd36a78f143d0
Tim Morgan <tim@timmorgan.org>
2014-09-29 14:19:10 -0500

    Make mouse scrolling work in the terminal
    I don't use the mouse much, but if my hand does land on it, I'd like for
    scrolling to work and to be able to place the cursor.
    
    I found this bit of elisp here: http://stackoverflow.com/a/8859057/197498

--- init.el
+++ init.el
@@ -132,3 +132,17 @@
   indent-tabs-mode nil)
 
 (define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; Enable mouse support
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+  )
